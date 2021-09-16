a67e81f6799f9348946542576e30c1fade587010
thisispawann <pawanstha.6964@gmail.com>
2021-09-16 23:32:54 +0545

    rendering post title to blog page

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 #Blog
 def Blog(request):
-    return render(request, 'blog.html')
+    
+    all_posts = Post.objects.all() #collected all data from post model
+    return render(request, 'blog.html', {'posts': all_posts})
 
 #Front end assets 
 def FrontEnd(request):
