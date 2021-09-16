e09a944cb5cd0fdf4238b0b0af4a32d5bc4b0654
thisispawann <pawanstha.6964@gmail.com>
2021-09-16 10:45:16 +0545

    data model created

---
+++ posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date='publish')
+    publish = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    status = models.CharField(max_length=255, choices=options, default='draft')
+    
+    
+    #showing title to the admin panel
+    def __str__(self):
+        return self.title
+    
