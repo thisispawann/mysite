5c732380b4a208c17dafba4aec975ddc9667975d
thisispawann <pawanstha.6964@gmail.com>
2021-09-17 11:53:46 +0545

    added table comment including comment fields to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-
+# Added meta data to admin area
+@admin.register(models.Post)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'slug', 'author')
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
 
-admin.site.register(models.Post, AuthorAdmin)
+#including comment to admin section
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'email', 'publish', 'status')
+    list_filter = ('status','publish')
+    search_fields = ('name', 'email', 'content')
--- posts/models.py
+++ posts/models.py
@@ -43,3 +43,20 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    
+   
+#Comment Table
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    content = models.TextField()
+    publish = models.DateTimeField(auto_now_add=True) 
+    status = models.BooleanField(default=True)
+    
+    class Meta: 
+        ordering = ('publish',)
+    
+    def __str__(self):
+        return f"Comment by {self.name}"   
+
